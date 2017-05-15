4a6897a26e74a40ffdd2e371aed1d3b884eb8627
codemac-02i <thanamolsa@gmail.com>
2017-05-15 15:47:55 +0530

    dhokkk
    ddddd

---
+++ myproject/myproject/detailesViewController.h
@@ -0,0 +1,15 @@
+//
+//  detailesViewController.h
+//  myproject
+//
+//  Created by codemac-02i on 5/15/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface detailesViewController : ViewController
+
+
+
+@end
--- myproject/myproject/tableViewController.h
+++ myproject/myproject/tableViewController.h
@@ -10,4 +10,8 @@
 
 @interface tableViewController : UIViewController
 
+
+
+
+
 @end
