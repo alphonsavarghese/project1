112aa3eb0678013ab7fb4491d538f6e8f99ecda7
Codemac-01i <alphonsavarghese22@gmail.com>
2017-05-15 15:31:08 +0530

    views added

---
+++ myproject/myproject/tableViewController.h
@@ -0,0 +1,13 @@
+//
+//  tableViewController.h
+//  myproject
+//
+//  Created by Codemac-01i on 15/05/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tableViewController : UIViewController
+
+@end
---
+++ myproject/myproject/tableViewController.m
@@ -0,0 +1,37 @@
+//
+//  tableViewController.m
+//  myproject
+//
+//  Created by Codemac-01i on 15/05/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import "tableViewController.h"
+
+@interface tableViewController ()
+
+@end
+
+@implementation tableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
