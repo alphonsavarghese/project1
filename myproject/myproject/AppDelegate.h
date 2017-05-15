29a6c4a323eb7681262da6c0b1208460292d6b85
codemac-02i <thanamolsa@gmail.com>
2017-05-15 14:54:27 +0530

    lol

---
+++ myproject/myproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myproject
+//
+//  Created by codemac-02i on 5/15/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myproject/myproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myproject
+//
+//  Created by codemac-02i on 5/15/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
